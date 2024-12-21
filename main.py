9e3f0ecd248147564e31512a9a95444d963642f7
dahbimoad <dahbimoad1@gmail.com>
2024-12-21 04:51:29 +0100

    crud for post done , not tested yet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
+import schemas
 from database import Base,engine,get_db
 import models
 
@@ -15,12 +17,47 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Worldaaaaa"}
 
+@app.post("/posts/", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)  # Refresh to get the newly generated `id` and `created_at`
+    return new_post
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
-@app.post("/posts/create")
-def create_post(request_data : dict = Body(...)):
-    return {"post data:": f"title: {request_data['title']} + content: {request_data['content']}"}
+@app.get("/posts/", response_model=list[schemas.Post], status_code=status.HTTP_200_OK)
+def get_posts(db: Session = Depends(get_db)):
+    return db.query(models.Post).all()
 
+
+@app.get("/posts/{id}", response_model=schemas.Post, status_code=status.HTTP_200_OK)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
+@app.put("/posts/{id}", response_model=schemas.Post,)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
+
+
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post_query.delete(synchronize_session=False)
+    db.commit()
+    return {"detail": "Post deleted successfully"}
---
+++ schemas.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+# schema for the request that the server gets
+class PostCreate(PostBase):
+    pass
+
+#class PostUpdate(PostBase):
+
+#class PostDelete(PostBase):
+
+
+# schema for the response that the server sends
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    class Config:
+        orm_mode = True
