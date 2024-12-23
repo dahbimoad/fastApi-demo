7f74d95a1bcad4531ed5e24d55fd7c81cb25ccd7
dahbimoad <dahbimoad1@gmail.com>
2024-12-23 20:32:21 +0100

    now time for pipeline

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #this is : main.py
 from fastapi import FastAPI
 from jose import jwt
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.db.database import Base, engine
 from app.routers import auth, user, post
@@ -9,7 +9,13 @@ from app.routers import auth, user, post
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React development server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
