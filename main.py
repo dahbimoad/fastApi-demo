9a3a2a860ed25259d34e887b49419614163e8e16
dahbimoad <dahbimoad1@gmail.com>
2024-12-24 22:21:12 +0100

    Fix main.py for Heroku deployment

--- main.py
+++ main.py
@@ -26,10 +26,3 @@ app.include_router(auth.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
-
